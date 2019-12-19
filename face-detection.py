3cadb2e01faca58f9de980d1d21798733151f27c
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2019-12-19 19:17:05 +0200

    Training the Recognizer

--- face-detection.py
+++ face-detection.py
@@ -1,6 +1,18 @@
 import cv2 # Import Open CV
+import numpy as np
+import pickle
+
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml") 
+
+labels = {}
+
+with open("labels.pickle", 'rb') as f:
+	originalLabels = pickle.load(f)
+	newLabels = {v:k for k,v in originalLabels.items()}
+
 
 capturingImages =cv2.VideoCapture(0) # Start capturing images.
 
@@ -11,10 +23,18 @@ while(True):  #Start an infinite loop of capturing images.
 	capturedFrameConvertedToGray = cv2.cvtColor(capturedFrame, cv2.COLOR_BGR2GRAY) #Conver the frame to gray because this is how this cascade works
 	facesFromCapturedFrameConvertedToGray = face_cascade.detectMultiScale(capturedFrameConvertedToGray, scaleFactor=1.5, minNeighbors = 5) #Values from documentation, I can modify them for better results, but this are defaults.
 	for (x, y, width, height) in facesFromCapturedFrameConvertedToGray:
-		print(x, y, width, height)
+		#print(x, y, width, height)
 		regionOfInterestForGray = capturedFrameConvertedToGray[y:y+height, x:x+width]
-		testImage = 'testImage.png'
-		cv2.imwrite(testImage, regionOfInterestForGray)
+		regionOfInterestColor = capturedFrame[y:y+height, x:x+width]
+
+		# Recognizer ? We can use deep lerned model predict
+		id_, confidence = recognizer.predict(regionOfInterestForGray)
+		if confidence >= 45 and confidence <=85:
+			print(id_)
+			print(newLabels[id_])
+
+		testImage = '10.png'
+		cv2.imwrite(testImage, regionOfInterestColor)
 
 		rectangleColor = (0,255,255) #Declaring the color of the rectangle in BGR 0-255
 		rectangleStroke = 2 #Rectangle stroke
