5410c4be28d6a5701552b64686509a135d70c350
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2019-12-19 18:17:12 +0200

    Converting images to grayscale and to NUMPY array

--- faces-train.py
+++ faces-train.py
@@ -1,8 +1,13 @@
 import os
+import numpy as np
+from PIL import Image
 
 baseDirectory = os.path.dirname(os.path.abspath(__file__)) #Where this file is saved we get the path.
 imageDirectory = os.path.join(baseDirectory, "images") #Looking for the images folder.
 
+
+y_labels= []
+x_train = []
 
 for root, directories, images in os.walk(imageDirectory):
 	for image in images:
@@ -10,5 +15,10 @@ for root, directories, images in os.walk(imageDirectory):
 			pathOfTheImage = os.path.join(root, image)
 			label = os.path.basename(root).replace(" ","-").lower() #Making sure the folder names are written as aaa-bbb.
 			print(label) #Printing the labels
+			#y_labels.append(label) #some number
+			#x_train.append(pathOfTheImage) #verify this image, turn into a NUMPY array, GRAY
+			pilImage = Image.open(pathOfTheImage).convert("L") #convert into grayscale
+			imageArray = np.array(pilImage, "uint8")
+			print(imageArray)
 
  
