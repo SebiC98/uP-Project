f76133dfb97e66a51d6e16af2a580680b285d2ac
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2019-12-19 18:37:17 +0200

    Recognizer

--- faces-train.py
+++ faces-train.py
@@ -2,39 +2,49 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+import pickle
 
 baseDirectory = os.path.dirname(os.path.abspath(__file__)) #Where this file is saved we get the path.
 imageDirectory = os.path.join(baseDirectory, "images") #Looking for the images folder.
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer =cv2.face.LBPHFaceRecognizer_create()
 
 currentId = 0;
 labelIds={}
 y_labels= []
-x_train = []
+trainigData = []
 
 for root, directories, images in os.walk(imageDirectory):
 	for image in images:
 		if image.endswith("png") or image.endswith("jpg"):
 			pathOfTheImage = os.path.join(root, image)
 			label = os.path.basename(root).replace(" ","-").lower() #Making sure the folder names are written as aaa-bbb.
-			print(label) #Printing the labels
-			if not label in labelIds:
+			#print(label) #Printing the labels
+			if not label in labelIds:  #Create an id for each person
 				labelIds[label]=currentId
 				currentId+=1
 			id_ = labelIds[label]
-			print(labelIds)
+			#print(labelIds)
 			#y_labels.append(label) #some number
-			#x_train.append(pathOfTheImage) #verify this image, turn into a NUMPY array, GRAY
+			#trainingData.append(pathOfTheImage) #verify this image, turn into a NUMPY array, GRAY
 			pilImage = Image.open(pathOfTheImage).convert("L") #convert into grayscale
 			imageArray = np.array(pilImage, "uint8") #convert into a NUMPY array. basically converting each picture into numbers. each pixel into a number.
-			print(imageArray)
+			#print(imageArray)
 			faceFromImage = face_cascade.detectMultiScale(imageArray, scaleFactor=1.5, minNeighbors = 5) #We have to detect the face in each picture.
 
 			for (x, y, width, height) in faceFromImage:
 				regionOfInterestForGray = imageArray[y:y+height, x:x+width]
-				x_train.append(regionOfInterestForGray)
+				trainingData.append(regionOfInterestForGray)
 				y_labels.append(id_)
 
 #print(y_labels)
-#print(x_train)
+#print(trainingData)
+
+
+with open("labels.pickle", 'wb') as f:
+	pickle.dump(labelIds, f)
+
+
+recognizer.train(trainigData, np.array(y_labels))
+recognizer.save("trainner.yml")
