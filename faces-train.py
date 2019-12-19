3b247ac2409b473c7a7dcd4e122f9ce1e3626379
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2019-12-19 18:30:02 +0200

    Creating IDs for Persons

--- faces-train.py
+++ faces-train.py
@@ -1,3 +1,4 @@
+import cv2
 import os
 import numpy as np
 from PIL import Image
@@ -5,7 +6,10 @@ from PIL import Image
 baseDirectory = os.path.dirname(os.path.abspath(__file__)) #Where this file is saved we get the path.
 imageDirectory = os.path.join(baseDirectory, "images") #Looking for the images folder.
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
+currentId = 0;
+labelIds={}
 y_labels= []
 x_train = []
 
@@ -15,10 +19,22 @@ for root, directories, images in os.walk(imageDirectory):
 			pathOfTheImage = os.path.join(root, image)
 			label = os.path.basename(root).replace(" ","-").lower() #Making sure the folder names are written as aaa-bbb.
 			print(label) #Printing the labels
+			if not label in labelIds:
+				labelIds[label]=currentId
+				currentId+=1
+			id_ = labelIds[label]
+			print(labelIds)
 			#y_labels.append(label) #some number
 			#x_train.append(pathOfTheImage) #verify this image, turn into a NUMPY array, GRAY
 			pilImage = Image.open(pathOfTheImage).convert("L") #convert into grayscale
-			imageArray = np.array(pilImage, "uint8")
+			imageArray = np.array(pilImage, "uint8") #convert into a NUMPY array. basically converting each picture into numbers. each pixel into a number.
 			print(imageArray)
+			faceFromImage = face_cascade.detectMultiScale(imageArray, scaleFactor=1.5, minNeighbors = 5) #We have to detect the face in each picture.
 
- 
+			for (x, y, width, height) in faceFromImage:
+				regionOfInterestForGray = imageArray[y:y+height, x:x+width]
+				x_train.append(regionOfInterestForGray)
+				y_labels.append(id_)
+
+#print(y_labels)
+#print(x_train)
